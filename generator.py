53e7e772942b510297cafaca261f6127882e945d
Eduardo Solano <105125779+proxlu@users.noreply.github.com>
2025-02-19 09:54:10 -0300

    Add files via upload

---
+++ generator.py
@@ -0,0 +1,48 @@
+import os
+import json
+from datetime import datetime
+
+def load_existing_index(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, "r", encoding="utf-8") as f:
+            return json.load(f)
+    return {"cache": {}, "metadata": {}}
+
+def generate_index():
+    base_dir = os.getcwd()
+    index_file = os.path.join(base_dir, "index.json")
+    script_name = os.path.basename(__file__).lower()
+    index_data = load_existing_index(index_file)
+    
+    existing_cache = index_data.get("cache", {})
+    
+    for root, _, files in os.walk(base_dir):
+        if root == base_dir:
+            continue
+        
+        dir_name = os.path.basename(root)
+        dir_key = dir_name.lower()
+        
+        if dir_key not in existing_cache:
+            existing_cache[dir_key] = {"_dirname": dir_name}
+        
+        for file in sorted(files, key=str.casefold):
+            if file.lower() in {script_name, "index.json"}:
+                continue
+            
+            name_without_ext, _ = os.path.splitext(file)
+            name_without_ext = name_without_ext.lower()
+            
+            if name_without_ext not in existing_cache[dir_key]:
+                existing_cache[dir_key][name_without_ext] = file
+    
+    index_data["cache"] = existing_cache
+    index_data["metadata"]["date"] = datetime.now().strftime("%Y-%m-%d")
+    
+    with open(index_file, "w", encoding="utf-8") as f:
+        json.dump(index_data, f, separators=(',', ':'), ensure_ascii=False, sort_keys=False)
+    
+    print("Arquivo index.json atualizado com sucesso!")
+
+if __name__ == "__main__":
+    generate_index()
